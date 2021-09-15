e93a995d31277d3e9f8ce564e55b82be6a3b6d72
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-09-15 19:07:57 +0800

    [new] winshift

--- config.lua
+++ config.lua
@@ -476,6 +476,33 @@ lvim.plugins = {
       end,
     },
     { "dhruvasagar/vim-zoom", keys = "<C-w>m" },
+    {
+      "sindrets/winshift.nvim",
+      cmd = "WinShift",
+      setup = function()
+        vim.cmd [[
+          nnoremap <C-W><C-M> <Cmd>WinShift<CR>
+          nnoremap <C-M-H> <Cmd>WinShift left<CR>
+          nnoremap <C-M-J> <Cmd>WinShift down<CR>
+          nnoremap <C-M-K> <Cmd>WinShift up<CR>
+          nnoremap <C-M-L> <Cmd>WinShift right<CR>
+        ]]
+      end,
+      config = function()
+        require("winshift").setup {
+          highlight_moving_win = true, -- Highlight the window being moved
+          focused_hl_group = "Visual", -- The highlight group used for the moving window
+          moving_win_options = {
+            -- These are local options applied to the moving window while it's
+            -- being moved. They are unset when you leave Win-Move mode.
+            wrap = false,
+            cursorline = false,
+            cursorcolumn = false,
+            colorcolumn = "",
+          },
+        }
+      end,
+    },
   },
   -- [Files]
   {
