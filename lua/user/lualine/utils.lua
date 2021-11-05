6abfcaab969c2406c6e594cc912cc248a3288de5
Xavier Young <45989017+younger-1@users.noreply.github.com>
2021-11-06 00:24:00 +0800

    [refine] own components

--- lua/user/lualine/utils.lua
+++ lua/user/lualine/utils.lua
@@ -1,10 +1,19 @@
 local conditions = require "lvim.core.lualine.conditions"
+local colors = require "lvim.core.lualine.colors"
 
 local M = {}
 
+M.mode = {
+  function()
+    return " "
+  end,
+  padding = { left = 0, right = 0 },
+  color = {},
+  cond = nil,
+}
+
 M.lock = {
   separator = { left = "", right = "" },
-  padding = 0,
   function()
     if not vim.o.modifiable then
       return ""
@@ -14,30 +23,136 @@ M.lock = {
       return ""
     end
   end,
+  padding = 0,
+  -- color = { fg = "#88c0d0", bg = "#bf616a" },
 }
 
-M.fileformat = { "fileformat", cond = conditions.hide_in_width }
+M.branch = {
+  "b:gitsigns_head",
+  icon = "",
+  color = { gui = "bold" },
+  cond = conditions.hide_in_width,
+}
 
-M.filesize = { "filesize", cond = conditions.hide_in_width }
+M.filename = {
+  "filename",
+  color = {},
+  cond = nil,
+}
+
+local function diff_source()
+  local gitsigns = vim.b.gitsigns_status_dict
+  if gitsigns then
+    return {
+      added = gitsigns.added,
+      modified = gitsigns.changed,
+      removed = gitsigns.removed,
+    }
+  end
+end
+
+M.diff = {
+  "diff",
+  source = diff_source,
+  symbols = { added = " ", modified = "柳 ", removed = " " },
+  diff_color = {
+    added = { fg = colors.green },
+    modified = { fg = colors.yellow },
+    removed = { fg = colors.red },
+  },
+  color = {},
+  cond = nil,
+}
+
+M.diagnostics = {
+  "diagnostics",
+  sources = { "nvim_lsp" },
+  symbols = { error = " ", warn = " ", info = " ", hint = " " },
+  color = {},
+  cond = conditions.hide_in_width,
+}
+
+M.treesitter = {
+  function()
+    local b = vim.api.nvim_get_current_buf()
+    if next(vim.treesitter.highlighter.active[b]) then
+      return ""
+    end
+    return ""
+  end,
+  color = { fg = colors.green },
+  cond = conditions.hide_in_width,
+}
+
+M.filetype = {
+  "filetype",
+  cond = conditions.hide_in_width,
+  color = {},
+}
 
 M.tab_space = {
   function()
     if not vim.api.nvim_buf_get_option(0, "expandtab") then
-      return "TAB: " .. vim.api.nvim_buf_get_option(0, "tabstop")
+      return "TAB:" .. vim.api.nvim_buf_get_option(0, "tabstop")
     end
     local size = vim.api.nvim_buf_get_option(0, "shiftwidth")
     if size == 0 then
       size = vim.api.nvim_buf_get_option(0, "tabstop")
     end
-    return "SPC: " .. size
+    return "SPC:" .. size
   end,
   cond = conditions.hide_in_width,
   color = {},
+  padding = { left = 1, right = 0 },
 }
 
-M.trailing_white = function()
-  return vim.fn.search([[\s\+$]], "nw") ~= 0 and "TW" or ""
-end
+M.fileformat = {
+  "fileformat",
+  cond = conditions.hide_in_width,
+  padding = { left = 1, right = 0 },
+}
+
+M.filesize = {
+  "filesize",
+  cond = conditions.hide_in_width,
+  padding = { left = 1, right = 0 },
+}
+
+M.encoding = {
+  "o:encoding",
+  fmt = string.upper,
+  color = {},
+  cond = conditions.hide_in_width,
+  padding = { left = 1, right = 0 },
+}
+
+M.trailing_white = {
+  function()
+    return vim.fn.search([[\s\+$]], "nw") ~= 0 and "TW" or ""
+  end,
+  padding = { left = 1, right = 0 },
+}
+
+M.location = {
+  "location",
+  cond = conditions.hide_in_width,
+  padding = { left = 1, right = 0 },
+  color = {},
+}
+
+M.scrollbar = {
+  function()
+    local current_line = vim.fn.line "."
+    local total_lines = vim.fn.line "$"
+    local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+    local line_ratio = current_line / total_lines
+    local index = math.ceil(line_ratio * #chars)
+    return chars[index]
+  end,
+  padding = { left = 0, right = 0 },
+  color = { fg = colors.yellow, bg = colors.bg },
+  cond = nil,
+}
 
 M.get_short_cwd = function()
   return vim.fn.fnamemodify(vim.fn.getcwd(), ":~")
